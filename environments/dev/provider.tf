db93bb9cc40ad3be16813a02ee6e6bff1ea4bee1
vikramadityayuvi <vikramaditya.yuvi@gmail.com>
2025-09-14 00:05:22 +0530

    changed 4

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,21 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.44.0"
     }
   }
 
   backend "azurerm" {
-    resource_group_name  = "testrg"
-    storage_account_name = "testsg"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-    
+    resource_group_name   = "testrg"
+    storage_account_name  = "testsg"
+    container_name        = "tfstate"
+    key                   = "dev.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "76095b6e-0585-402e-8e43-281835aebfd1"
 }
